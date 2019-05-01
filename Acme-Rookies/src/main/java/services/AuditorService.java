73bf9f458b6ac4857d4a553a079fe02fb727114d
alexaur97 <ugarte97aur@gmail.com>
2019-05-01 15:26:54 +0200

    auditor

--- Acme-Rookies/src/main/java/services/AuditorService.java
+++ Acme-Rookies/src/main/java/services/AuditorService.java
@@ -1,70 +1,139 @@
-package services; 
 
-import java.util.Collection; 
+package services;
 
-import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.stereotype.Service; 
-import org.springframework.transaction.annotation.Transactional; 
-import org.springframework.util.Assert; 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import miscellaneous.Utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.AuditorRepository;
+import security.Authority;
+import security.UserAccount;
+import domain.Auditor;
+import domain.CreditCard;
+import domain.Position;
+import forms.AuditorRegisterForm;
 
-import domain.Auditor; 
-
-@Service 
-@Transactional 
-public class AuditorService { 
+@Service
+@Transactional
+public class AuditorService {
 
 	//Managed repository -------------------
 	@Autowired
-	private AuditorRepository auditorRepository;
+	private AuditorRepository	auditorRepository;
+
+	@Autowired
+	private ActorService		actorService;
 
 
 	//Supporting Services ------------------
 
-
 	//COnstructors -------------------------
-	public AuditorService(){
+	public AuditorService() {
 		super();
 	}
 
-
 	//Simple CRUD methods--------------------
 
-	public Auditor create(){
+	public Auditor create() {
 		Auditor result;
-
 		result = new Auditor();
+		result.setBanned(false);
+		result.setSpammer(false);
+
+		final Collection<Position> positions = new ArrayList<>();
+		result.setPositions(positions);
+
+		final CreditCard creditCard = new CreditCard();
+		result.setCreditCard(creditCard);
+
+		final UserAccount userAccount = new UserAccount();
+		result.setUserAccount(userAccount);
+
+		final Authority a = new Authority();
+		a.setAuthority(Authority.AUDITOR);
+		final Collection<Authority> authorities = new ArrayList<Authority>();
+		authorities.add(a);
+		result.getUserAccount().setAuthorities(authorities);
 
 		return result;
 	}
 
-	public Collection<Auditor> findAll(){
+	public Collection<Auditor> findAll() {
 		Collection<Auditor> result;
 
-		result = auditorRepository.findAll();
+		result = this.auditorRepository.findAll();
 
 		return result;
 	}
 
-	public Auditor findOne(int auditorId){
+	public Auditor findOne(final int auditorId) {
 		Auditor result;
 
-		result = auditorRepository.findOne(auditorId);
+		result = this.auditorRepository.findOne(auditorId);
 
 		return result;
 	}
 
-	public void save(Auditor auditor){
+	public void save(final Auditor auditor) {
 		Assert.notNull(auditor);
 
-		auditorRepository.save(auditor);
+		this.auditorRepository.save(auditor);
 	}
 
-	public void delete(Auditor auditor){
-		auditorRepository.delete(auditor);
+	public void delete(final Auditor auditor) {
+		this.auditorRepository.delete(auditor);
 	}
 
+	public Auditor constructByForm(final AuditorRegisterForm auditorRegisterForm) {
+		Assert.isTrue(auditorRegisterForm.getPassword().equals(auditorRegisterForm.getConfirmPassword()));
+		final Auditor result = this.create();
+		final Collection<String> emails = this.actorService.findAllEmails();
+		final String email = auditorRegisterForm.getEmail();
+		final boolean bEmail = !emails.contains(email);
+		Assert.isTrue(bEmail);
+
+		final Collection<String> accounts = this.actorService.findAllAccounts();
+		final UserAccount userAccount = result.getUserAccount();
+		final Boolean bAccount = !accounts.contains(auditorRegisterForm.getUsername());
+		Assert.isTrue(bAccount);
+
+		final Md5PasswordEncoder pe = new Md5PasswordEncoder();
+		final String password = pe.encodePassword(auditorRegisterForm.getPassword(), null);
+		userAccount.setPassword(password);
+		userAccount.setUsername(auditorRegisterForm.getUsername());
+		result.setUserAccount(userAccount);
+		result.setAddress(auditorRegisterForm.getAddress());
+		result.setBanned(false);
+
+		final CreditCard creditCard = result.getCreditCard();
+		creditCard.setBrandName(auditorRegisterForm.getBrandName());
+		creditCard.setCvv(auditorRegisterForm.getCvv());
+		creditCard.setExpirationMonth(auditorRegisterForm.getExpirationMonth());
+		creditCard.setExpirationYear(auditorRegisterForm.getExpirationYear());
+		creditCard.setHolderName(auditorRegisterForm.getHolderName());
+		creditCard.setNumber(auditorRegisterForm.getNumber());
+		final Boolean b = Utils.creditCardIsExpired(creditCard);
+		Assert.isTrue(!b);
+		result.setCreditCard(creditCard);
+
+		result.setEmail(auditorRegisterForm.getEmail());
+		result.setName(auditorRegisterForm.getName());
+		result.setPhone(this.actorService.addCountryCode(auditorRegisterForm.getPhone()));
+		result.setPhoto(auditorRegisterForm.getPhoto());
+		result.setSpammer(false);
+		result.setSurnames(auditorRegisterForm.getSurnames());
+
+		final String vat = auditorRegisterForm.getVAT();
+		result.setVAT(vat.toUpperCase());
+		return result;
+	}
 
 	//Other Methods--------------------
-} 
+}
--- Acme-Rookies/src/main/webapp/views/master-page/header.jsp
+++ Acme-Rookies/src/main/webapp/views/master-page/header.jsp
@@ -16,8 +16,8 @@
 	uri="http://www.springframework.org/security/tags"%>
 
 <div>
-	<a href="#"><img src="images/logo.png"
-		alt="Acme Rookies Co., Inc." style="margin-bottom: 0.5em;" /></a>
+	<a href="#"><img src="images/logo.png" alt="Acme Rookies Co., Inc."
+		style="margin-bottom: 0.5em;" /></a>
 </div>
 
 <div>
@@ -34,18 +34,19 @@
 		</security:authorize>
 		<li><a class="fNiv" href="position/search.do"><spring:message
 					code="master.page.searchPosition" /></a></li>
-					
+
 		<security:authorize access="hasRole('ROOKIE')">
-			<li><a href="finder/rookie/view.do"><spring:message code="master.page.finder" /></a></li>
+			<li><a href="finder/rookie/view.do"><spring:message
+						code="master.page.finder" /></a></li>
 		</security:authorize>
 
 		<security:authorize access="hasRole('COMPANY')">
 			<li><a class="fNiv" href="position/company/myList.do"><spring:message
 						code="master.page.myPositions" /></a></li>
-						<li><a class="fNiv" href="application/company/list.do"><spring:message
+			<li><a class="fNiv" href="application/company/list.do"><spring:message
 						code="master.page.myApplications" /></a></li>
 		</security:authorize>
-<security:authorize access="hasRole('ROOKIE')">
+		<security:authorize access="hasRole('ROOKIE')">
 			<li><a class="fNiv" href="application/rookie/list.do"><spring:message
 						code="master.page.myApplications" /></a></li>
 		</security:authorize>
@@ -68,38 +69,41 @@
 						property="principal.username" />)
 			</a>
 				<ul>
-				
+
 					<li class="arrow"></li>
-		                    <security:authorize access="hasRole('ADMINISTRATOR')">
-		            <li><a href="administrator/administrator/create.do"><spring:message code="master.page.signup.admin" /></a></li>
-		                    </security:authorize>
-		             
-		                <security:authorize  access="hasRole('ADMINISTRATOR')" >
-		            <li><a href="actor/editAdm.do"><spring:message
-								code="master.page.editProfile" /></a></li>
-							</security:authorize>
-				
-				<security:authorize access="hasAnyRole('ROOKIE','COMPANY')">
-						   <li><a href="actor/edit.do"><spring:message
-								code="master.page.editProfile" /></a></li>
-				</security:authorize>
 					<security:authorize access="hasRole('ADMINISTRATOR')">
-					
-					<li><a href="actor/administrator/list.do"><spring:message
-								code="master.page.actorList" /></a></li>
-		            </security:authorize>
+						<li><a href="administrator/administrator/create.do"><spring:message
+									code="master.page.signup.admin" /></a></li>
+						<li><a href="auditor/administrator/create.do"><spring:message
+									code="master.page.signup.auditor" /></a></li>
+					</security:authorize>
+
+					<security:authorize access="hasRole('ADMINISTRATOR')">
+						<li><a href="actor/editAdm.do"><spring:message
+									code="master.page.editProfile" /></a></li>
+					</security:authorize>
+
+					<security:authorize access="hasAnyRole('ROOKIE','COMPANY')">
+						<li><a href="actor/edit.do"><spring:message
+									code="master.page.editProfile" /></a></li>
+					</security:authorize>
+					<security:authorize access="hasRole('ADMINISTRATOR')">
+
+						<li><a href="actor/administrator/list.do"><spring:message
+									code="master.page.actorList" /></a></li>
+					</security:authorize>
 
 					<security:authorize access="hasRole('ROOKIE')">
-					<li><a href="curriculum/rookie/list.do"><spring:message
-								code="master.page.curriculum" /> </a></li>
+						<li><a href="curriculum/rookie/list.do"><spring:message
+									code="master.page.curriculum" /> </a></li>
 					</security:authorize>
 					<li><a href="message/list.do"><spring:message
 								code="master.page.message" /> </a></li>
-								
+
 					<li><a href="socialprofile/list.do"><spring:message
 								code="master.page.socialProfile" /> </a></li>
-								
-							
+
+
 					<li><a href="j_spring_security_logout"><spring:message
 								code="master.page.logout" /> </a></li>
 
