c192ccd76c8c1ab5c94323b40ee9225800f69b7c
Andrea <andreadommon12345@gmail.com>
2019-05-14 07:57:45 +0200

    con fallo editAuditTest

--- Acme-Rookies/src/test/java/service/AuditServiceTest.java
+++ Acme-Rookies/src/test/java/service/AuditServiceTest.java
@@ -24,27 +24,29 @@ public class AuditServiceTest extends AbstractTest {
 	private AuditService	auditService;
 
 
-	//Requisito 3.2 Un Actor autenticado como Auditor puede administrar sus auditorias.
+	//Requisito 3.2 Un Actor autenticado como Auditor puede editar sus auditorias.
 
 	@Test
 	public void testEditAuditGood() {
 		super.authenticate("auditor1");
 		final int IdAudit = super.getEntityId("audit1");
 		Audit audit = this.auditService.findOne(IdAudit);
+		System.out.println(audit.getMode());
 		audit.setText("text");
 		audit.setScore(2);
 		audit.setMode("FINAL");
 
+		System.out.println(audit.getMode());
 		audit = this.auditService.reconstruct(audit, null);
 
 		this.auditService.save(audit);
 		super.unauthenticate();
 	}
-	//	Para el caso negativo estamos intentando que un Auditor se asigne una posicion en modo borrador
-	//Análisis del sentence coverage: el sistema al llamar al metodo del servicio "reconstruct" comprueba
-	// que la position seleccionada cumpla todas las condiciones.
+	//	Para el caso negativo estamos intentando que un Auditor edite una auditoria en modo borrador
+	//Análisis del sentence coverage: el sistema al llamar al metodo del servicio "save" comprueba
+	// que la auditoria anteriormente estuviera en modo borrador.
 
-	@Test
+	@Test(expected = NullPointerException.class)
 	public void testEditAuditError() {
 		super.authenticate("auditor1");
 		final int IdAudit = super.getEntityId("audit2");
@@ -56,6 +58,32 @@ public class AuditServiceTest extends AbstractTest {
 		audit = this.auditService.reconstruct(audit, null);
 
 		this.auditService.save(audit);
+		super.unauthenticate();
+	}
+
+	//Requisito 3.2 Un Actor autenticado como Auditor puede borrar sus auditorias.
+
+	@Test
+	public void testDeleteAuditGood() {
+		super.authenticate("auditor1");
+		final int IdAudit = super.getEntityId("audit1");
+		final Audit audit = this.auditService.findOne(IdAudit);
+
+		this.auditService.delete(audit);
+		super.unauthenticate();
+	}
+
+	//	Para el caso negativo estamos intentando que un Auditor elimine una auditoria en modo borrador
+	//Análisis del sentence coverage: el sistema al llamar al metodo del servicio "save" comprueba
+	// que la auditoria anteriormente estuviera en modo borrador.
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testDeleteAuditError() {
+		super.authenticate("auditor1");
+		final int IdAudit = super.getEntityId("audit2");
+		final Audit audit = this.auditService.findOne(IdAudit);
+
+		this.auditService.delete(audit);
 		super.unauthenticate();
 	}
 
